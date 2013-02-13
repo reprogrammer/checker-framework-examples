59d212ef1c06955a41fe6421f468c5cf3528b895
Mohsen Vakilian <reprogrammer@gmail.com>
2013-02-12 19:11:50 -0600

    Added the Enumerate example.

--- programs/enumerate/src/Enumerate.java
+++ programs/enumerate/src/Enumerate.java
@@ -1,4 +1,44 @@
+import checkers.nullness.quals.*;
+
+class Node {
+  int id;
+  @Nullable Node next;
+
+  Node(int id, @Nullable Node next) {
+    this.id = id;
+    this.next = next;
+  }
+}
 
 class Enumerate {
+  private @Nullable Node current;
 
+  public Enumerate(Node node) {
+    this.current = node;
+  }
+
+  @AssertNonNullIfTrue("current")
+  public boolean hasMoreElements() {
+    return (current != null);
+  }
+
+  @NonNullOnEntry("current")
+  public @Nullable Node nextElement() {
+    Node retval = current;
+    current = current.next;
+    return retval;
+  }
 }
+
+class Main {
+  public static final void main(String args[]) {
+    Node n2 = new Node(2, null);
+    Node n1 = new Node(1, n2);
+    Enumerate e = new Enumerate(n1);
+    while (e.hasMoreElements()) {
+      @NonNull Node n = e.nextElement();
+      System.out.println(n.id);
+    }
+  }
+}
+
