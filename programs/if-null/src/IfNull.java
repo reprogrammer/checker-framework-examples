cd6ddc99c5ee5c73bf54598ee362657e8dbd3b3a
Mohsen Vakilian <reprogrammer@gmail.com>
2013-02-18 15:13:11 -0600

    Added a program to test an issue in perimeter.

---
+++ programs/if-null/src/IfNull.java
@@ -0,0 +1,16 @@
+import checkers.nullness.quals.Nullable;
+
+class Main {
+  @Nullable Object o1, o2;
+  void m() {
+    if (o1 == null || o2 == null) {
+      return;
+    } else {
+      System.out.println(o1.toString() + o2.toString());
+    }
+  }
+  public static final void main(String args[]) {
+    new Main().m();
+  }
+}
+
